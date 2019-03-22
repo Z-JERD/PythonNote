e5d2ebcd6774934a8e2a488e013f89f961b48ea8
zhaoguangfei <zhaoguangfei@qq.com>
2019-03-23 00:00:19 +0800

    linux操作

--- LOGDEMO/DEMO/app.py
+++
@@ -1,8 +0,0 @@
-import logging
-from config.config import *
-setup_logging()
-logger = logging.getLogger("root")
-if __name__ == '__main__':
-    logger.info("this is a demo")
-    logger.error("this is a error")
-
